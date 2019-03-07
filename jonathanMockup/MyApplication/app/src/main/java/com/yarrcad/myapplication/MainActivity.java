fdba3c478feaf490f5503a6305cff723684d5918
Yarrcad <jonathancardinale@gmail.com>
2019-03-07 11:00:34 -0800

    Uploaded Mockup

---
+++ jonathanMockup/MyApplication/app/src/main/java/com/yarrcad/myapplication/MainActivity.java
@@ -0,0 +1,90 @@
+package com.yarrcad.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private ListView mListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        ArrayList<Card> list = new ArrayList<>();
+
+        list.add(new Card("drawable://" + R.drawable.smokey_mountain, "Monday"));
+        list.add(new Card("drawable://" + R.drawable.smokey_mountain, "Tuesday"));
+        list.add(new Card("drawable://" + R.drawable.smokey_mountain, "Wednesday"));
+        list.add(new Card("drawable://" + R.drawable.smokey_mountain, "Thursday"));
+        list.add(new Card("drawable://" + R.drawable.smokey_mountain, "Friday"));
+
+        CustomListAdapter adapter = new CustomListAdapter(this, R.layout.content_main, list);
+        mListView.setAdapter(adapter);
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
+        Date d = new Date();
+        setTitle("Today Is: " + sdf.format(d));
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.myWeek) {
+            // Handle the camera action
+        } else if (id == R.id.prevWeeks) {
+
+        } else if (id == R.id.settings) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
